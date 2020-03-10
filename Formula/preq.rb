1328238c526ac4dfea7e32c08762f8d944caf534
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-10 08:17:17 +0100

    Brew formula update for preq version v0.0.2-rc6

--- Formula/preq.rb
+++ Formula/preq.rb
@@ -2,16 +2,16 @@
 class Preq < Formula
   desc ""
   homepage ""
-  version "0.0.2-rc4"
+  version "0.0.2-rc6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tfrs1/preq/releases/download/v0.0.2-rc4/preq_0.0.2-rc4_Darwin_x86_64.tar.gz"
-    sha256 "e1dc9a45dfa4b5a1edbd98a52f20a0383639e1edda4ac216057260d58376ee38"
+    url "https://github.com/tfrs1/preq/releases/download/v0.0.2-rc6/preq_0.0.2-rc6_Darwin_x86_64.tar.gz"
+    sha256 "8f9eee65557f2539e924ff643970fd7f2b664ad2560158ab22744254c27d3294"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tfrs1/preq/releases/download/v0.0.2-rc4/preq_0.0.2-rc4_Linux_x86_64.tar.gz"
-      sha256 "61c628fd7bb2b8a7cd6cd4ffd757685178baa5b344a6526d636838de9ff02102"
+      url "https://github.com/tfrs1/preq/releases/download/v0.0.2-rc6/preq_0.0.2-rc6_Linux_x86_64.tar.gz"
+      sha256 "8145f0a7c26f19d05fe9972d4c0556733fd112fe020949ddb0e4c13aabc5dd3f"
     end
   end
 
